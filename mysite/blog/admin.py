aea28d832b10a06227dc163143d579f34cbe56d8
Халмурзиев Микаил <>
2022-06-08 20:50:37 +0300

    Добавление Комментариев

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
-@admin.register(Post)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
-    list_filter = ['status', 'created', 'publish', 'author']
-    search_fields = ['title', 'body']
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ['author']
-    date_hierarchy = 'publish'
-    ordering = ['status', 'publish']
+    
+
+
+admin.site.register(Post, PostAdmin)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,41 +1,37 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-
-        return super().get_queryset() \
-            .filter(status=Post.Status.PUBLISHED)
-
-
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+ 
 class Post(models.Model):
-    class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-
-    PUBLISHED = 'PB', 'Published'
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2,
-                              choices=Status.choices,
-                              default=Status.DRAFT)
-    objects = models.Manager()  # The default manager.
-    published = PublishedManager()  # Our custom manager.
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
-        ordering = ['-publish']
-
-    indexes = [
-        models.Index(fields=['-publish']),
-    ]
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
